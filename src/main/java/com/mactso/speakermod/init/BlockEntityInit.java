0dc9fb469b6df045b305cf93e802ea769d6fa4ce
mactso <stephenmcleod2004@yahoo.com>
2022-09-06 15:31:39 -0500

    Add the register method to BlockEntityInit

--- src/main/java/com/mactso/speakermod/init/BlockEntityInit.java
+++ src/main/java/com/mactso/speakermod/init/BlockEntityInit.java
@@ -4,6 +4,7 @@ import com.mactso.speakermod.Main;
 import com.mactso.speakermod.blockentities.WirelessJukeboxBlockEntity;
 
 import net.minecraft.world.level.block.entity.BlockEntityType;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
@@ -11,6 +12,12 @@ import net.minecraftforge.registries.RegistryObject;
 public class BlockEntityInit {
     public static final DeferredRegister<BlockEntityType<?>> BLOCK_ENTITIES = DeferredRegister.create(ForgeRegistries.BLOCK_ENTITY_TYPES, Main.MODID);
 
-    public static final RegistryObject<BlockEntityType<WirelessJukeboxBlockEntity>> WIRELESS_JUKEBOX = BLOCK_ENTITIES.register("wireless_jukebox",
+    public static final RegistryObject<BlockEntityType<WirelessJukeboxBlockEntity>> WIRELESS_JUKEBOX = 
+    		BLOCK_ENTITIES.register("wireless_jukebox",
             () -> BlockEntityType.Builder.of(WirelessJukeboxBlockEntity::new, BlockInit.WIRELESS_JUKEBOX.get()).build(null));
+
+
+    public static void register (IEventBus eventBus) {
+    	BLOCK_ENTITIES.register(eventBus);
+    }
 }
